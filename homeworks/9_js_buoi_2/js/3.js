c0e2fe069184adcacbbbe55f1e88cfed1b992dea
quangthanh201 <tquangds@gmail.com>
2020-07-09 18:57:37 +0700

    buoi9_09072020

--- homeworks/9_js_buoi_2/js/3.js
+++ homeworks/9_js_buoi_2/js/3.js
@@ -1,13 +1,20 @@
-function checkFontSize() {
+var defaultFontSize = document.getElementById('my-p').style.fontSize;
+function setDefault() {
+	document.getElementById('my-p').style.fontSize = defaultFontSize;
+}
+function biggerFontSize() {
 	var font = document.getElementById('my-p').style.fontSize;
 	console.log(font);
-	font= font.substring(0, font.length-2);
+	font = font.substring(0, font.length-2);
 	font *= 1.4;
 	font += 'px';
 	document.getElementById('my-p').style.fontSize = font;
 }
-
-
-// font=substring(0,font.length-2);
-// 	font*=1.4
-// font+='px'
+function smallerFontSize() {
+	var font = document.getElementById('my-p').style.fontSize;
+	console.log(font);
+	font = font.substring(0, font.length-2);
+	font *= 0.6;
+	font += 'px';
+	document.getElementById('my-p').style.fontSize = font;
+}
