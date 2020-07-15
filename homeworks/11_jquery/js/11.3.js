48e3eae781af1fd622646c6a6b0ec66a0eef66f8
quangthanh201 <tquangds@gmail.com>
2020-07-15 20:27:01 +0700

    buoi11_15072020

--- homeworks/11_jquery/js/11.3.js
+++ homeworks/11_jquery/js/11.3.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
+	var x = 0;
 	$('#add').click(function() {
 		if ($('#new-work').val() == "") {
 			$('#warning').css('color', 'red');
 		} else {
 			$('#warning').css('color', '#8ac2c7');
-			$('.all-work').append('<div class="work" style="position: relative">' 
+			x += 1;
+			if (x % 2 == 0) {
+				$('.all-work').append('<div class="work" style="position: relative; background-color: #f9f9f9">' 
 				+ '<span>' + $('#new-work').val() + '</span>' 
 				+ '<i class="fa fa-times-circle" style="float: right; cursor: pointer" aria-hidden="true"></i>' 
 				+ '</div>');
+			} else {
+				$('.all-work').append('<div class="work" style="position: relative; background-color: #eeeeee">' 
+				+ '<span>' + $('#new-work').val() + '</span>' 
+				+ '<i class="fa fa-times-circle" style="float: right; cursor: pointer" aria-hidden="true"></i>' 
+				+ '</div>');
+			}
 		}
 		$('#new-work').val('');
+		console.log(x);
 	});
+	
 	$(document).on('click', '.fa-times-circle', function() {
 		$(this).parent().remove();
 	})
